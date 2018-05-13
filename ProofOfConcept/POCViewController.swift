24ded6adfc17261cbe03f65340e296c1a62cf934
Shweta Shendage <ShwetaShendage@Shrikants-MacBook-Air.local>
2018-05-13 16:44:50 +0530

    first commit

---
+++ ProofOfConcept/POCViewController.swift
@@ -0,0 +1,25 @@
+//
+//  POCViewController.swift
+//  ProofOfConcept
+//
+//  Created by Shweta Shendage on 13/05/18.
+//  Copyright © 2018 Shweta Shendage. All rights reserved.
+//
+
+import UIKit
+
+class POCViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "POC"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
